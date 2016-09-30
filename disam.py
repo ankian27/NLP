e961fc12c84999b4ccca136cb0a5044f7e2f977c
Brandon paulsen <pauls658@umn.edu>
2016-09-30 17:23:06 -0500

    Got a start on reading in the xml files

--- disam.py
+++ disam.py
@@ -1,11 +1,24 @@
-from src import context
+from src.context import Context
 import xmltodict
 import optparse
 import sys
 
+"""
+Disambiguates the given sense2eval xml file, f. The target parameter is only necessary if
+the target word isn't marked with <head> tags.
+@param f: the file path to the sense2eval xml formatted file
+@param target: optional target word
+"""
 def disam(f, target=None):
     with open(f, 'r') as xml_file:
-        xml = xmltodict.parse(f.read())
+        xml = xmltodict.parse(xml_file.read())
+    for instance in xml['corpus']['lexelt']['instance']:
+        #ctx_ref = Context(instance.context)
+        print instance['context']
+        #print ctx_ref.context
+        #print ctx_ref.target
+        break
+
 
 if __name__ == '__main__':
     parser = optparse.OptionParser(description='Disambiguate a target word.')
@@ -17,5 +30,5 @@ if __name__ == '__main__':
     if not f:
         print "need an xml file to parse"
         sys.exit(1)
-    disam(f[0], options['target'])
+    disam(f[0], options.target)
 
