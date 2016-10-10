0e4f346d2ed03abd0b04519d7d36524061d35272
Ankit Anand Gupta <ankian27@gmail.com>
2016-10-10 11:47:59 -0500

    adjectives added

--- src/DefinitionGeneration.py
+++ src/DefinitionGeneration.py
@@ -13,11 +13,11 @@ class Definition(object):
 		self.cfgRule=defaultdict(list)
 		self.noun = ''
 		self.verb = ''
-
+		self.adj  = ''
 	def get_Noun_Verb(self, topics):
 		self.noun = ''
 		self.verb = ''
-		adj=[]
+		self.adj  = ''
 		adv=[]
 		# det=['the','a','is','are',]
 		# CONJ='and'
@@ -34,14 +34,16 @@ class Definition(object):
 				# verb.append(word)
 				self.verb += word+'|'
 			if tag=='ADJ':
-				adj.append(word)
+				self.adj += word+'|'
 			if tag=='ADV':
 				adv.append(word)
-		print self.noun, self.verb
+		print self.noun, self.verb ,self.adj
 		self.noun=self.noun[:-1]
 		
 		self.verb=self.verb[:-1]
-		return self.noun, self.verb
+
+		self.adj=self.adj[:-1]
+		return self.noun, self.verb ,self.adj
 
 	def cfg_rule(self,left,right):
 
@@ -51,7 +53,7 @@ class Definition(object):
 
 	def gen_def(self, symbol):
 		definition = ''
-		#del_word
+		
 		rule = random.choice(self.cfgRule[symbol])
 		for sym in rule:
 			if sym in self.cfgRule:
@@ -80,15 +82,16 @@ class Definition(object):
 
 	def generate_Definition(self, topics, target):
  		topics = [topic for topic in topics if target not in topic]
- 		self.noun, self.verb = self.get_Noun_Verb(topics)
-        	self.cfg_rule('S', 'S1 CONJ S2')
+ 		self.noun, self.verb ,self.adj= self.get_Noun_Verb(topics)
+		self.cfg_rule('S', 'S1 CONJ S2')
 		self.cfg_rule('S1', 'NP VP')
 		self.cfg_rule('S2', 'NP VP')
 		self.cfg_rule('NP', 'Det N')
-		self.cfg_rule('VP', 'V PRO NP')
+		self.cfg_rule('VP', 'V PRO ADJ NP')
 		self.cfg_rule('CONJ','and')
 		self.cfg_rule('PRO','with | to')
 		self.cfg_rule('Det', 'a | the | is')
 		self.cfg_rule('N', self.noun)
 		self.cfg_rule('V', self.verb)
+		self.cfg_rule('ADJ', self.adj)
 		return self.gen_def('S')	
