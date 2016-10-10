8891d295ec3f1b3fbd848cfaa345b6ecd3addc31
Ankit Anand Gupta <ankian27@gmail.com>
2016-10-09 20:43:23 -0500

    Added Defenition Generation

---
+++ testPOS.py
@@ -0,0 +1,4 @@
+from pos_tag import Definition
+# from pos_tag.definition import generate_Definition
+definition = Definition()
+definition.generate_Definition(set(['grip', 'play', 'ball', 'player', 'tennis' ,'players' ,'racket' ,'table' ,'side' ,'match']), 'racket')
