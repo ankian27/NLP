e68f2d132fc117979a44f19e10de24d790bfea9d
Brandon <pauls658@d.umn.edu>
2016-12-07 17:46:23 -0600

    asljkghadfjklghajklhvgjkodfhkljfghasdjklhfasdjklhgaslgkljashg

--- stage2/cluster_baseline.py
+++ stage2/cluster_baseline.py
@@ -68,227 +68,97 @@ def cluster_tfidfs(file_name, model, pref=None):
         ctxes_tfidf[i] = filter(lambda x: x[0] in model, ctxes_tfidf[i])
 
     final_ctxes = []
-<<<<<<< HEAD
-<<<<<<< HEAD
-    final_ctxes_pos = []
-    for i, ctx in enumerate(raw_ctxes):
-        final_ctxes.append(filter(lambda x: x in model and x in vocab, ctx))
-        final_ctxes_pos.append(filter(lambda x: x[0] in model and x[0] in vocab, pos_ctxes[i]))
+    for ctx in ctxes_tfidf:
+        final_ctxes.append(ctx[:WORD_VEC_SIZE])
 
-    ctx_vecs = make_context_vecs(final_ctxes, model)
-    pref = np.max(pdist(ctx_vecs))
-    pref = float(-1) * pref * pref
-    ap = AffinityPropagation(damping=0.5, convergence_iter=15, max_iter=300, preference=pref).fit(ctx_vecs)
+    ctx_vecs = make_context_vecs_tfidf(final_ctxes, model)
+    prefs = []
+    if not pref:
+        pref = np.max(pdist(ctx_vecs))
+        pref = float(-1) * pref * pref
+        inc = PREF_INC * pref
+        for i in range(-5, 5):
+            prefs.append(inc*float(i) + pref)
+    else:
+        # debugging
+        prefs.append(pref)
+    
+    if len(ctx_vecs) > PRIMING_SIZE:
+        # list index -> instance id
+        li_to_ii = random.sample(range(len(ctx_vecs)), PRIMING_SIZE)
+        priming_sample = [ctx_vecs[ii] for ii in li_to_ii]
+    else:
+        priming_sample = ctx_vecs
 
-    clusters = get_clusters(ap, senses, final_ctxes_pos)
+    ch_scores = []
+    all_clusters = []
+    for ferp in prefs:
+        ap = AffinityPropagation(damping=0.75, convergence_iter=50, max_iter=1000, preference=ferp).fit(priming_sample)
+    
+        num_clusters = len(set(ap.labels_))
+        if num_clusters > MAX_CLUSTERS:
+            continue
+        if num_clusters != 1 and num_clusters != len(priming_sample):
+            ch_score = metrics.calinski_harabaz_score(priming_sample, ap.labels_)
+        else:
+            # scoring metrics can't operate on one cluster
+            ch_score = 0
+        ch_scores.append(ch_score)
+        all_clusters.append(copy.copy(ap.labels_))
 
-    definition = Definition(model, pos)
-    for i, cluster in enumerate(clusters):
+    best_cluster = all_clusters[ch_scores.index(max(ch_scores))]
+    instance_clusters = get_clusters(best_cluster)
+    final_cluster = []
+    if len(priming_sample) != len(ctx_vecs):
+        # keep track of the contexts we clustered already
+        # index in ctx_vecs -> cluster label
+        assigned = {ii:best_cluster[li] for li, ii in enumerate(best_cluster)}
+        # make a cluster vector for each cluster
+        cluster_vecs = []
+        for ctx_ids in instance_clusters:
+            cluster_vecs.append(sum([priming_sample[i] for i in ctx_ids]) / float(len(ctx_ids)))
+        # assign the rest of our contexts
+        for i, vec in enumerate(ctx_vecs):
+            if i in assigned:
+                final_cluster.append(assigned[i])
+            else:
+                best_match = -1
+                max_sim = -2.0
+                for i, cluster_vec in enumerate(cluster_vecs):
+                    sim = np.dot(vec, cluster_vec)/(np.linalg.norm(vec)* np.linalg.norm(cluster_vec))
+                    if sim > max_sim:
+                        max_sim = sim
+                        best_match = i
+                final_cluster.append(best_match)
+    else:
+        final_cluster = best_cluster
+
+    score_cluster(final_cluster, file_name, target_word, pos)
+
+    final_clusters = get_clusters(final_cluster)
+    defgen_model = Word2Vec.load('models/3_parts_of_wiki_lowercase')
+    definition = Definition(defgen_model, pos)
+    for i, cluster in enumerate(final_clusters):
         word_counts = defaultdict(int)
         doc_counts = defaultdict(int)
-        for _, context in cluster:
+        for instance_id in cluster:
             # word is the (word, pos) tuple
             seen = set()
-            for word in context:
-            	seen.add(word)
-                word_counts[word] += 1
+            for word in pos_ctxes[instance_id]:
+                if word[0] in defgen_model:
+                    word_counts[word] += 1
+                    seen.add(word)
             for word in seen:
-            	doc_counts[word] += 1
-
+                doc_counts[word] += 1
         word_counts_list = []
         for (word, pos), count in word_counts.iteritems():
             word_counts_list.append((word, pos, count))
         word_counts_list = sorted(word_counts_list, key=lambda x: x[2], reverse=True)
-        # print 'Cluster ' + str(i)
-        # print '---------'
-        # print '\n'.join(word + ' ' + pos + ' ' + str(count) for word, pos, count in word_counts_list)
         defPhrase = definition.process(word_counts_list, doc_counts)
         print defPhrase
-
-    # os.system('rm senseclusters_scorer/answers*; rm senseclusters_scorer/key*')
-    make_answers(ap, target_word, pos)
-    make_key(file_name, target_word, pos)
-    # os.system('cd senseclusters_scorer; ./senseclusters_scorer.sh answers key; cd ..')
-    # os.system('cat senseclusters_scorer/report.out')
+    sys.stdout.flush()
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print "usage: python def_gen.py <input-dir>"
-        sys.exit(1)
-    main(sys.argv[1])
-    # for f_name in os.listdir(sys.argv[1]):
-    # 	main(sys.argv[1] + '/' + f_name)
-=======
-    for ctx in ctxes_tfidf:
-        final_ctxes.append(ctx[:WORD_VEC_SIZE])
-
-    ctx_vecs = make_context_vecs_tfidf(final_ctxes, model)
-    prefs = []
-    if not pref:
-        pref = np.max(pdist(ctx_vecs))
-        pref = float(-1) * pref * pref
-        inc = PREF_INC * pref
-        for i in range(-5, 5):
-            prefs.append(inc*float(i) + pref)
-    else:
-        # debugging
-        prefs.append(pref)
-    
-    if len(ctx_vecs) > PRIMING_SIZE:
-        # list index -> instance id
-        li_to_ii = random.sample(range(len(ctx_vecs)), PRIMING_SIZE)
-        priming_sample = [ctx_vecs[ii] for ii in li_to_ii]
-    else:
-        priming_sample = ctx_vecs
-
-    ch_scores = []
-    all_clusters = []
-    for ferp in prefs:
-        ap = AffinityPropagation(damping=0.75, convergence_iter=50, max_iter=1000, preference=ferp).fit(priming_sample)
-    
-        num_clusters = len(set(ap.labels_))
-        if num_clusters > MAX_CLUSTERS:
-            continue
-        if num_clusters != 1 and num_clusters != len(priming_sample):
-            ch_score = metrics.calinski_harabaz_score(priming_sample, ap.labels_)
-        else:
-            # scoring metrics can't operate on one cluster
-            ch_score = 0
-        ch_scores.append(ch_score)
-        all_clusters.append(copy.copy(ap.labels_))
-
-    best_cluster = all_clusters[ch_scores.index(max(ch_scores))]
-    instance_clusters = get_clusters(best_cluster)
-    final_cluster = []
-    if len(priming_sample) != len(ctx_vecs):
-        # keep track of the contexts we clustered already
-        # index in ctx_vecs -> cluster label
-        assigned = {ii:best_cluster[li] for li, ii in enumerate(best_cluster)}
-        # make a cluster vector for each cluster
-        cluster_vecs = []
-        for ctx_ids in instance_clusters:
-            cluster_vecs.append(sum([priming_sample[i] for i in ctx_ids]) / float(len(ctx_ids)))
-        # assign the rest of our contexts
-        for i, vec in enumerate(ctx_vecs):
-            if i in assigned:
-                final_cluster.append(assigned[i])
-            else:
-                best_match = -1
-                max_sim = -2.0
-                for i, cluster_vec in enumerate(cluster_vecs):
-                    sim = np.dot(vec, cluster_vec)/(np.linalg.norm(vec)* np.linalg.norm(cluster_vec))
-                    if sim > max_sim:
-                        max_sim = sim
-                        best_match = i
-                final_cluster.append(best_match)
-    else:
-        final_cluster = best_cluster
-    score_cluster(final_cluster, file_name, target_word, pos)
-
-#    definition = Definition(model)
-#    for i, cluster in enumerate(clusters):
-#        word_counts = defaultdict(int)
-#        for _, context in cluster:
-#            # word is the (word, pos) tuple
-#            for word in context:
-#                word_counts[word] += 1
-#        word_counts_list = []
-#        for (word, pos), count in word_counts.iteritems():
-#            word_counts_list.append((word, pos, count))
-#        word_counts_list = sorted(word_counts_list, key=lambda x: x[2], reverse=True)
-#        # print 'Cluster ' + str(i)
-#        # print '---------'
-#        # print '\n'.join(word + ' ' + pos + ' ' + str(count) for word, pos, count in word_counts_list)
-#        defPhrase = definition.process(word_counts_list)
-#        print defPhrase
-
-if __name__ == '__main__':
-=======
-    for ctx in ctxes_tfidf:
-        final_ctxes.append(ctx[:WORD_VEC_SIZE])
-
-    ctx_vecs = make_context_vecs_tfidf(final_ctxes, model)
-    prefs = []
-    if not pref:
-        pref = np.max(pdist(ctx_vecs))
-        pref = float(-1) * pref * pref
-        inc = PREF_INC * pref
-        for i in range(-5, 5):
-            prefs.append(inc*float(i) + pref)
-    else:
-        # debugging
-        prefs.append(pref)
-    
-    if len(ctx_vecs) > PRIMING_SIZE:
-        # list index -> instance id
-        li_to_ii = random.sample(range(len(ctx_vecs)), PRIMING_SIZE)
-        priming_sample = [ctx_vecs[ii] for ii in li_to_ii]
-    else:
-        priming_sample = ctx_vecs
-
-    ch_scores = []
-    all_clusters = []
-    for ferp in prefs:
-        ap = AffinityPropagation(damping=0.75, convergence_iter=50, max_iter=1000, preference=ferp).fit(priming_sample)
-    
-        num_clusters = len(set(ap.labels_))
-        if num_clusters > MAX_CLUSTERS:
-            continue
-        if num_clusters != 1 and num_clusters != len(priming_sample):
-            ch_score = metrics.calinski_harabaz_score(priming_sample, ap.labels_)
-        else:
-            # scoring metrics can't operate on one cluster
-            ch_score = 0
-        ch_scores.append(ch_score)
-        all_clusters.append(copy.copy(ap.labels_))
-
-    best_cluster = all_clusters[ch_scores.index(max(ch_scores))]
-    instance_clusters = get_clusters(best_cluster)
-    final_cluster = []
-    if len(priming_sample) != len(ctx_vecs):
-        # keep track of the contexts we clustered already
-        # index in ctx_vecs -> cluster label
-        assigned = {ii:best_cluster[li] for li, ii in enumerate(best_cluster)}
-        # make a cluster vector for each cluster
-        cluster_vecs = []
-        for ctx_ids in instance_clusters:
-            cluster_vecs.append(sum([priming_sample[i] for i in ctx_ids]) / float(len(ctx_ids)))
-        # assign the rest of our contexts
-        for i, vec in enumerate(ctx_vecs):
-            if i in assigned:
-                final_cluster.append(assigned[i])
-            else:
-                best_match = -1
-                max_sim = -2.0
-                for i, cluster_vec in enumerate(cluster_vecs):
-                    sim = np.dot(vec, cluster_vec)/(np.linalg.norm(vec)* np.linalg.norm(cluster_vec))
-                    if sim > max_sim:
-                        max_sim = sim
-                        best_match = i
-                final_cluster.append(best_match)
-    else:
-        final_cluster = best_cluster
-    score_cluster(final_cluster, file_name, target_word, pos)
-
-#    definition = Definition(model)
-#    for i, cluster in enumerate(clusters):
-#        word_counts = defaultdict(int)
-#        for _, context in cluster:
-#            # word is the (word, pos) tuple
-#            for word in context:
-#                word_counts[word] += 1
-#        word_counts_list = []
-#        for (word, pos), count in word_counts.iteritems():
-#            word_counts_list.append((word, pos, count))
-#        word_counts_list = sorted(word_counts_list, key=lambda x: x[2], reverse=True)
-#        # print 'Cluster ' + str(i)
-#        # print '---------'
-#        # print '\n'.join(word + ' ' + pos + ' ' + str(count) for word, pos, count in word_counts_list)
-#        defPhrase = definition.process(word_counts_list)
-#        print defPhrase
-
-if __name__ == '__main__':
->>>>>>> b6f78d3dca6d1c6cd1681036188e34544ceba195
 
     print "Loading model"
     model = Word2Vec.load_word2vec_format(model_file, binary=True)
@@ -296,7 +166,3 @@ if __name__ == '__main__':
 
     for file_name in os.listdir('input'):
         cluster_tfidfs('input/' + file_name, model)
-<<<<<<< HEAD
->>>>>>> b6f78d3dca6d1c6cd1681036188e34544ceba195
-=======
->>>>>>> b6f78d3dca6d1c6cd1681036188e34544ceba195
