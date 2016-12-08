294a2b04689fd88dbce6853131acc3e874820ed5
Brandon <pauls658@d.umn.edu>
2016-12-07 18:41:29 -0600

    Added documentation to Brandon's code

--- stage2/buildModel.py
+++
@@ -1,9 +0,0 @@
-from gensim.models import Word2Vec
-from src.sentenceIter import SentenceIter
-
-si = SentenceIter('sentences')
-
-model = Word2Vec(si, min_count=15, size=200, workers=4)
-
-print "Done training model. Writing it out."
-model.save('3_parts_of_wiki_lowercase')
